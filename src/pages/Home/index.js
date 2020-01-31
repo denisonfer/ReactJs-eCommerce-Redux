86643f9d8d9326f8c03c0a24f8abe9affc718054
Denison Ferreira <denisonferreira@MacBook-Pro-de-Denison.local>
2020-01-31 20:26:18 -0300

    Primeiro commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import api from '../../services/api';
+import * as CartAction from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
+import { ProductList } from './styles';
+
+class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
+
+    addToCartRequest(id);
+  };
+
+  render() {
+    const { products } = this.state;
+    const { amount } = this.props;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
+
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product.id)}
+            >
+              <div>
+                <MdAddShoppingCart size={24} color="#fff" />
+                {amount[product.id] || 0}
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(CartAction, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
